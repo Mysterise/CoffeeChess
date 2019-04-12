867409d31c96458da2c89a37bb26bc5ba4701896
Tony Tang <tonytang215@hotmail.com>
2019-04-13 01:05:26 +1000

    Removed redundant lines

--- src/CoffeeChess/UI/Board/BoardDisplay.java
+++ src/CoffeeChess/UI/Board/BoardDisplay.java
@@ -11,16 +11,13 @@ public class BoardDisplay extends Pane {
     public static final int NUM_ROWS = 8;
 
     private Group squareGroup = new Group();
-    private Group pieceGroup = new Group();
 
     public BoardDisplay() {
-        this.getChildren().addAll(squareGroup, pieceGroup);
+        this.getChildren().add(squareGroup);
         for (int row = 0; row < NUM_ROWS; row++) {
             for (int column = 0; column < NUM_COLUMNS; column++) {
                 SquareDisplay squareDisplay = new SquareDisplay((column + row) % 2 == 0, new Position(column, row));
-//                PieceDisplay piece = new PieceDisplay(new Position(column, row));
                 squareGroup.getChildren().add(squareDisplay);
-//                pieceGroup.getChildren().addAll(piece);
             }
         }
     }
